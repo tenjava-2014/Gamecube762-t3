7f4cd6006c875c375797dbb2f37a1511f31fa6ee
Kyle Mollart <Gamecube762@hotmail.com>
2014-07-12 11:47:13 -0700

    Added "tractor beam" to the VillagerAbduction event

--- src/main/java/com/tenjava/entries/Gamecube762/t3/Events/VillagerAbduction.java
+++ src/main/java/com/tenjava/entries/Gamecube762/t3/Events/VillagerAbduction.java
@@ -3,6 +3,7 @@ package com.tenjava.entries.Gamecube762.t3.Events;
 import com.tenjava.entries.Gamecube762.t3.TenJava;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -51,7 +52,7 @@ public class VillagerAbduction {
                 }
 
             }
-        }, 20, 10);
+        }, 20, 5);
     }
 
     public void findVillagers(Player p, double range) {
@@ -66,9 +67,43 @@ public class VillagerAbduction {
 
     public void abductVillager(Villager villager) {
         villagersBeingAbducted.put(villager, villager.getLocation());
+
+        for (Entity entity : villager.getNearbyEntities(50,50,50))
+            if (entity instanceof Player)
+                BuildLiftBeam((Player)entity, villager.getLocation());
     }
 
     public void abductedVilager(Villager villager) {
         villagersBeingAbducted.remove(villager);
     }
+
+
+    /*
+        0,-1,0  - glass  - 1
+        0,-2,0  - beacon - 2
+        0,-3,0  - iron   - 3
+        0,-3,1  - iron   - 4
+        0,-3,-1 - iron   - 5
+        1,-3,0  - iron   - 6
+        1,-3,1  - iron   - 7
+        1,-3,-1 - iron   - 8
+        */
+    static private HashMap<Integer, Material> a = new HashMap<Integer, Material>();
+    static private HashMap<Integer, Vector>   b = new HashMap<Integer, Vector>();
+    static {
+        a.put(1, Material.GLASS);       b.put(1, new Vector(0,-1,0));
+        a.put(2, Material.BEACON);      b.put(2, new Vector(0,-2,0));
+        a.put(3, Material.IRON_BLOCK);  b.put(3, new Vector(0,-3,0));
+        a.put(4, Material.IRON_BLOCK);  b.put(4, new Vector(0,-3,1));
+        a.put(5, Material.IRON_BLOCK);  b.put(5, new Vector(0,-3,-1));
+        a.put(6, Material.IRON_BLOCK);  b.put(6, new Vector(1,-3,0));
+        a.put(7, Material.IRON_BLOCK);  b.put(7, new Vector(1,-3,1));
+        a.put(8, Material.IRON_BLOCK);  b.put(8, new Vector(1,-3,-1));
+    }
+
+    public static void BuildLiftBeam(Player p, Location l) {
+        for (int i = 1; i <= 8; i++)
+            p.sendBlockChange(l.add(b.get(i)), a.get(i), (byte)0);
+    }
+
 }
